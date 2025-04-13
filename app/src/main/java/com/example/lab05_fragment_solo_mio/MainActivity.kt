52c9ecf802327f43d28dd2f8dfd0e32f5677e5af
giallo123 <66916719+giallo123@users.noreply.github.com>
2025-04-13 19:36:57 +0200

    Initial commit

---
+++ app/src/main/java/com/example/lab05_fragment_solo_mio/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.lab05_fragment_solo_mio
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.findNavController
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val navController = findNavController(R.id.my_nav_host_fragment)
+        navController.navigate(R.id.action_welcomeFragment_to_detailFragment)
+    }
+}
